dfe11a6098035cb596e4c12bbf1f94b2f0f9ff75
Akash-Kumar-Prasad <iakashprasad@gmail.com>
2024-02-24 16:36:02 +0530

    basic

---
+++ basic.js
@@ -0,0 +1,14 @@
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greetings = "Hello"
+console.log(greetings);
+console.log([1,2,3,4,5]);
+console.log({a:1, b:2});
+console.table({a:3,b:5});
+console.error('There is some error ');
+//console.clear();
+console.warn("warning");
+console.time("Hellow")
+console.log("1");
+console.timeEnd("Hellow")
