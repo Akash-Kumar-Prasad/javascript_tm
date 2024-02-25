22fb9f550b817a4076a550ab294eb3e39ed11c3f
Akash-Kumar-Prasad <iakashprasad@gmail.com>
2024-02-25 05:58:04 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,30 @@
+//Primitive
+
+//Strings
+const fname = "Jahn Doe"
+//number
+const age = 45
+//boolean
+const hasKids = true
+//null
+const car = null
+//undefined
+let test;
+//symbol
+const sym = Symbol()
+
+//Reference 
+//array
+const hobbies = ['movies', 'music']
+
+//object literal
+const address = {
+  city:"Kolkata",
+  state:"wb"
+}
+console.log(typeof address);
+
+//constructor
+const today = new Date();
+console.log(today);
+console.log(typeof today);
