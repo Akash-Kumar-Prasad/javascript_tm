9abd0effe6476ddb165fbef43d3795a0c2924b97
Akash-Kumar-Prasad <iakashprasad@gmail.com>
2024-03-10 20:38:20 +0530

    scope js

---
+++ scope.js
@@ -0,0 +1,29 @@
+// GLOBAL SCOPE
+
+var a = 1;
+let b = 2;
+const c = 3;
+
+// function test(){
+//   var a = 4;
+// let b = 5;
+// const c = 6;
+// console.log('Function scope ', a, b, c);
+// }
+
+// test() 
+
+// if(true){
+//   //block scope
+//   var a = 4;
+// let b = 5;
+// const c = 6;
+// console.log('if scope ', a, b, c);
+// }
+
+for(var a=0;a<10;a++){
+  console.log(`loop ${a}`);
+}
+
+console.log('global scope', a, b,c);
+
