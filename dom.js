6489b9b718521449e20c8161332ecddb535e0eb7
Akash-Kumar-Prasad <iakashprasad@gmail.com>
2024-03-10 22:45:37 +0530

    dom

---
+++ dom.js
@@ -0,0 +1,22 @@
+// DOCUMENT OBJECT MODEL
+//tree of nodes/ elements created by the browser
+//javascript can be used to read / write/ manipulate the DOM
+//object oriented representation 
+
+let val;
+
+val=document;
+val=document.all;
+val=document.all[0]
+val=document.evaluate.length;
+val=document.head;
+val=document.doctype;
+val=document.body
+val=document.domain
+val=document.URL;
+val=document.characterSet
+val=document.contentType
+val=document.forms;
+val=document.forms[0]
+val=document.form[0].id
+console.log(val);
